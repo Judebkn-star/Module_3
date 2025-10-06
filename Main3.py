495caa05c5d607ee749ee318d5ada0a11e672177
Jude Buyikana <jbuyikana@eugeniaschool.com>
2025-10-06 13:14:12 +0000

    Ma version

--- Main3.py
+++ Main3.py
@@ -1,1 +1,28 @@
-clear
+# TODO: Create a string variable with a sentence
+
+sentence = "Je suis Jude"
+print(sentence)
+
+# TODO: Convert the string to uppercase
+
+sentence_upper = sentence.upper()
+print(sentence_upper)
+
+# TODO: Convert the string to lowercase
+
+sentence_lower = sentence.lower()
+print(sentence_lower)
+
+# TODO: Capitalize the first letter of each word
+sentence_title = sentence.title()
+print(sentence_title)
+
+# TODO: Replace a word in the string
+
+sentence_replaced = sentence.replace("Je", "Tu").replace("Jude","Moi")
+print(sentence_replaced)
+
+
+
+# EXERCICE 2 MODULE 1
+
